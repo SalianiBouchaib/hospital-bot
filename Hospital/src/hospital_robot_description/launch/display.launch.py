7f7e5936cb578791a3d66c2b39e0f7e2d5f6b194
zaimi yassine <zaimi.yass@gmail.com>
2026-06-14 23:35:15 +0100

    Add files via upload

---
+++ Hospital/src/hospital_robot_description/launch/display.launch.py
@@ -0,0 +1,35 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    xacro_file = PathJoinSubstitution(
+        [FindPackageShare("hospital_robot_description"), "urdf", "robot.urdf.xacro"]
+    )
+    robot_description = {"robot_description": Command(["xacro ", xacro_file])}
+
+    return LaunchDescription(
+        [
+            Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                name="robot_state_publisher",
+                output="screen",
+                parameters=[robot_description],
+            ),
+            Node(
+                package="joint_state_publisher",
+                executable="joint_state_publisher",
+                name="joint_state_publisher",
+                output="screen",
+            ),
+            Node(
+                package="rviz2",
+                executable="rviz2",
+                name="rviz2",
+                output="screen",
+            ),
+        ]
+    )

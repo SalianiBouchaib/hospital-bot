8180c7278aa18345adefa98918e6f9acebf3c247
wassimm22331 <wasstrachli@gmail.com>
2026-06-14 22:42:00 +0100

    uploaded  robot delivery
    worked on the robot delivery system

---
+++ hospital_robot_delivery/share/hospital_robot_delivery/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/mnt/c/Users/MSI/Desktop/project robotv2 prime-trial/project robotv2/hospital_robot_ws/build/hospital_robot_delivery"
